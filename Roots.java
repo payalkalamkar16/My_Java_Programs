d2ad4b89ab0cf04b700dc8a2981ae24e4d6f50cf
payalkalamkar16 <154056321+payalkalamkar16@users.noreply.github.com>
2024-04-02 09:06:31 +0530

    Create Roots.java

---
+++ Roots.java
@@ -0,0 +1,21 @@
+public class Roots
+{
+public void sqr()
+{
+int a=6,b;
+b=a*a;
+System.out.println("Square is:"+b);
+}
+public void cube()
+{
+int a=9,b;
+b=a*a*a;
+System.out.println("Cube is:"+b);
+}
+public static void main(String args[])
+{
+Roots a=new Roots();
+a.sqr();
+a.cube();
+}
+}
