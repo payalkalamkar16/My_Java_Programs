1c9805108b0007aad588cae68bc67370569a899c
payalkalamkar16 <154056321+payalkalamkar16@users.noreply.github.com>
2024-04-06 19:29:59 +0530

    Create Add_of_Num.java

---
+++ Add_of_Num.java
@@ -0,0 +1,14 @@
+public class Addofnum
+{
+public static void main(String args[])
+{
+int i=1,add=0;
+do
+{
+add=add+i;
+i++;
+}
+while(i<=50);
+System.out.println("Addition of 1 to 50 numbers is:"+add);
+}
+}
