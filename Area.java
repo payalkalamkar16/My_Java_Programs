a7b4f95703cc6d9c38fa3fe2f5edc48ad8fe9ada
payalkalamkar16 <154056321+payalkalamkar16@users.noreply.github.com>
2024-04-08 15:45:08 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,14 @@
+public class Circle
+{
+public void Area()
+{
+double r=5,b;
+b=3.14*r*r;
+System.out.println("Area of circle is:"+b);
+}
+public static void main(String args[])
+{
+Circle a1=new Circle();
+a1.Area();
+}
+}
