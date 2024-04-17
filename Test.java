9af3f5f4be886db61c18c0d1a6ea1ddb5d408a5e
payalkalamkar16 <154056321+payalkalamkar16@users.noreply.github.com>
2024-04-17 17:59:10 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Test
+{
+public static void main(String args[])
+{
+int Num;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the Number:");
+Num=s.nextInt();
+if(Num>0)
+{
+System.out.println("Number is Positive");3
+}
+else
+{
+System.out.println("Number is Negative");
+}
+}
+} 
